0120e45aec4f2b6861ef6d3de1da0c5fd1a99e2f
CAKE <CAKEGamingHub@gmail.com>
2015-03-21 14:26:09 +0000

    JL
    Done: False
    Compile: True
    AMW: True

--- src/playerObjDef.py
+++ src/playerObjDef.py
@@ -8,5 +8,31 @@ class playerObjDef:
 
         self.direction = 0 #Bearing
         self.speed = 0
-        
-        
+        self.solid = True
+        self.visible = True
+
+    def move(self):
+        if self.direction == 0: self.y -= self.speed #Straight up
+        if self.direction == 90: self.x += self.speed #Right
+        if self.direction == 180: self.y += self.speed #Down
+        if self.direction == 270: self.x -= self.speed #Left
+
+        if self.diection == 45:
+            self.x += self.speed/2
+            self.y -= self.speed/2
+        if self.direction == 135:
+            self.x += self.speed/2
+            self.y += self.speed/2
+        if self.direction == 225:
+            self.x -= self.speed/2
+            self.y += self.speed/2
+        if self.direction == 315:
+            self.x -= self.speed/2
+            self.y -= self.speed/2
+
+    def invis(self): self.visible = False
+    def vis(self): self.visible = True
+
+    def desolid(self): self.solid = False
+    def solidify(self): self.solid = True
+    
