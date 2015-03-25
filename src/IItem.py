c55cab310277f9bb68f81043c7ff22ba74cb199c
CAKE <CAKEGamingHub@gmail.com>
2015-03-25 20:24:23 +0000

    JL
    Done: False
    Compiles: True
    AMW: Not Tested

---
+++ src/IItem.py
@@ -0,0 +1,10 @@
+### Item Template Def ###
+
+class IItem:
+    def __init__(self, Cost, SellVal, Dmg, Effect):
+        self.cost = Cost
+        self.sellVal = SellVal
+        self.dmg = Dmg
+        self.effect = Effect
+
+    
